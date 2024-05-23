dff4e85609dafdb541919ddae49117ba835cfbc3
abhishek kumar <abhisingh2084@gmail.com>
2024-05-23 11:32:53 +0530

    Bootraps added

--- InstallingBootstrap/Program.cs
+++ InstallingBootstrap/Program.cs
@@ -8,6 +8,8 @@ namespace InstallingBootstrap
             builder.Services.AddControllersWithViews();
             var app = builder.Build();
 
+            app.UseStaticFiles();
+
             //app.MapGet("/", () => "Hello World!");
 
             app.MapControllerRoute(
